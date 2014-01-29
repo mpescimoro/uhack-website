003d4d7f6446adcfafc75e7cb7cdde241321c336
Giovanni Caniato <giocaniato@gmail.com>
2014-01-29 20:40:48 +0100

    no login per cercare post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate!, except: [:show, :index]
+  before_action :authenticate!, except: [:show, :index, :search]
 
   # GET /posts
   # GET /posts.json
