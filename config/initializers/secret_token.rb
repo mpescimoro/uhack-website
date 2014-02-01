9b7908eb27992b69e3feb26208624244e7273212
Giovanni Caniato <giocaniato@gmail.com>
2014-02-01 13:41:58 +0100

    secret token is now actually secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Uhack::Application.config.secret_key_base = '261b42c301f668ae1616f75e1c2e27f7c7c102d3a5f5967a3c272a5e533784e22f24259215cb78746ad02b43d9d00a175810e62815574fde18e9c3032a5876a2'
+Uhack::Application.config.secret_key_base = ENV['SECRET_TOKEN'] || '261b42c301f668ae1616f75e1c2e27f7c7c102d3a5f5967a3c272a5e533784e22f24259215cb78746ad02b43d9d00a175810e62815574fde18e9c3032a5876a2'
