2c30f24c53c202e4125c0c2a4cbe1383c515a6d8
Giovanni Caniato <giocaniato@gmail.com>
2014-05-25 10:16:27 +0200

    tolto azioni e view inutili sui commenti

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,22 +2,10 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
   before_action :set_commentable, only: [:create, :edit, :update]
   before_action :authenticate_comment_commenter!, only: [:edit, :update, :destroy]
-# before_action :authenticate_commenter!, only: [:create]
-
-  # GET /comments
-  # GET /comments.json
-  def index
-    @comments = Comment.all
-  end
 
   # GET /comments/1
   # GET /comments/1.json
   def show
-  end
-
-  # GET /comments/new
-  def new
-    @comment = Comment.new
   end
 
   # GET /comments/1/edit
